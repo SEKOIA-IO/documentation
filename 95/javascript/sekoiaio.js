a6b2c9eca0d27b7a1751333010bd9f7b9d8dcf32
GitHub Action <action@github.com>
2021-04-15 08:16:19 +0000

    Remove files for PR 95

--- 95/javascript/sekoiaio.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-    var divs = document.querySelectorAll('h2');
-
-    [].forEach.call(divs, function(div) {
-        let p = document.createElement("span");
-        div.prepend(p);
-    });
-});
