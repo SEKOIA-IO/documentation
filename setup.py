f42cbbee6225dde5e4bf5c78ea0bfe3e7f3a08df
Mickael Decouflet <mickael.decouflet@sekoia.fr>
2021-11-06 22:19:44 +0100

    feat(githubaction): add step for install redoc plugin

--- setup.py
+++ setup.py
@@ -3,24 +3,16 @@ import setuptools
 
 setuptools.setup(
     name='redoc',
-    version='0.5.1',
-    description='Plugin for adding monorepository support in Mkdocs.',
-    long_description="""
-        This introduces support for the !include syntax in mkdocs.yml, allowing you to import additional Mkdocs navigation.
-        It enables large or complex repositories to have their own sets of docs/ folders, whilst generating only a single Mkdocs site.
-        This is built and maintained by the engineering community at Spotify.
-    """,  # noqa: E501
-    keywords='mkdocs monorepo',
-    url='https://github.com/backstage/mkdocs-monorepo-plugin',
-    author='Bilawal Hameed',
-    author_email='bil@spotify.com',
+    version='0.0.1',
+    description='Plugin for adding redoc in Mkdocs.',
+    long_description='redoc plugin on mkdocs',  # noqa: E501
+    keywords='redoc',
+    url='',
+    author='Mickael Découflet',
+    author_email='mickael.decouflet@sekoia.fr',
     license='Apache-2.0',
     python_requires='>=3',
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
