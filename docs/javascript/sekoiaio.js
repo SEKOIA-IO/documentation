418b989b46a011dc16b3b46c8da98a68f2f0bf5e
Georges Bossert <georges.bossert@sekoia.fr>
2022-01-12 08:48:15 +0100

    Update docs/javascript/sekoiaio.js
    Co-authored-by: Gwen <gwen.gpn@gmail.com>

--- docs/javascript/sekoiaio.js
+++ docs/javascript/sekoiaio.js
@@ -6,10 +6,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
         div.prepend(p);
     });
 
-    var uls = document.getElementsByClassName("md-tabs__list");
-    if (uls.length != 0) {
-	uls[0].insertAdjacentHTML("beforeend", "<li class='md-tabs__item'><a data-sleek data-badge-changelog class='md-tabs__link' href='#'>Changelog</a></li>");
-    }
+    const ul = document.querySelector('.md-tabs__list');
+    ul?.insertAdjacentHTML("beforeend", "<li class='md-tabs__item'><a data-sleek data-badge-changelog class='md-tabs__link' href='#'>Changelog</a></li>");
 
     window.$sleek=[];window.SLEEK_PRODUCT_ID=901462981;(function(){d=document;s=d.createElement("script");s.src="https://client.sleekplan.com/sdk/e.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
 });
