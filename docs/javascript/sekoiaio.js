44059df51a334588abf689d67a14a97fb63deb8a
Olivier Tétard <olivier.tetard@sekoia.fr>
2022-06-30 22:11:15 +0200

    Remove auto-num of h2 titles

--- docs/javascript/sekoiaio.js
+++ docs/javascript/sekoiaio.js
@@ -1,11 +1,4 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    var divs = document.querySelectorAll('h2');
-
-    [].forEach.call(divs, function(div) {
-        let p = document.createElement("span");
-        div.prepend(p);
-    });
-
     const ul = document.querySelector('.md-tabs__list');
     ul?.insertAdjacentHTML("beforeend", "<li class='md-tabs__item'><a data-sleek data-badge-changelog class='md-tabs__link' href='#'>Changelog</a></li>");
 
