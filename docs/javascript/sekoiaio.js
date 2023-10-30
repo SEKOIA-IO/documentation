e57af5ec12c4be6185734eb10594df90cf3311ae
Mathieu Bellon <github@mathieubellon.fr>
2023-10-30 14:57:02 +0100

    Fix changelog menu

--- docs/javascript/sekoiaio.js
+++ docs/javascript/sekoiaio.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     const ul = document.querySelector('.md-tabs__list');
-    ul?.insertAdjacentHTML("beforeend", "<li class='md-tabs__item'><a data-sleek data-badge-changelog class='md-tabs__link' href='#'>Changelog</a></li>");
+    ul?.insertAdjacentHTML("beforeend", "<li class='md-tabs__item'><a class='md-tabs__link' href='https://changelog.sekoia.io' target='_blank'>Changelog</a></li>");
 
     var elements = document.getElementsByClassName("lightgallery");
     for(var i=0; i<elements.length; i++) {
