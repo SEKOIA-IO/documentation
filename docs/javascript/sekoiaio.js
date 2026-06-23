db7509e3856246240b7e80340c47f1b0b61f5fb3
Olivier Tétard <olivier.tetard@sekoia.io>
2026-06-23 10:33:01 +0200

    feat: Add SEO

--- docs/javascript/sekoiaio.js
+++ docs/javascript/sekoiaio.js
@@ -7,3 +7,5 @@ window.addEventListener("DOMContentLoaded", (event) => {
         lightGallery(elements[i]);
     }
 });
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://ksxk.sekoia.com/4j0bfgtunkswv.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','bvoy7=HxNOLDo4WSk1OUBcP0BSEUtFXVpIERpaBRIYBAcUWgQZGg%3D%3D');
